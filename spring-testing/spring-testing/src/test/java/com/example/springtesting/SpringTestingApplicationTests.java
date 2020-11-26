00195d680eaf2a4a33997f1036d262852e3ea5e4
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2020-11-26 19:43:44 -0300

    intial commit

---
+++ spring-testing/spring-testing/src/test/java/com/example/springtesting/SpringTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
