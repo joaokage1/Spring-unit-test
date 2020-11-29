a19d460f36b9495ae975d9135fed024f96195248
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2020-11-29 17:04:55 -0300

    Update ListMockTest.java

--- spring-testing/spring-testing/src/test/java/com/example/springtesting/business/ListMockTest.java
+++ spring-testing/spring-testing/src/test/java/com/example/springtesting/business/ListMockTest.java
@@ -2,42 +2,58 @@ package com.example.springtesting.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class ListMockTest {
 
-	@SuppressWarnings("rawtypes")
+	@SuppressWarnings("unchecked")
+	@InjectMocks
+	List<String> mock = Mockito.mock(List.class);
+
 	@Test
 	public void basic_size() {
-		List mock = Mockito.mock(List.class);
-		when(mock.size()).thenReturn(5);
 
-		assertEquals(5, mock.size());
+		when(this.mock.size()).thenReturn(5);
+
+		assertEquals(5, this.mock.size());
 	}
 
-	@SuppressWarnings("rawtypes")
 	@Test
 	public void different_values() {
-		List mock = Mockito.mock(List.class);
-		when(mock.size()).thenReturn(5).thenReturn(10);
+		when(this.mock.size()).thenReturn(5).thenReturn(10);
 
-		assertEquals(5, mock.size());
-		assertEquals(10, mock.size());
+		assertEquals(5, this.mock.size());
+		assertEquals(10, this.mock.size());
 	}
 
-	@SuppressWarnings("rawtypes")
 	@Test
 	public void withGenerParameter_values() {
-		List mock = Mockito.mock(List.class);
-		when(mock.get(anyInt())).thenReturn("toddy");
+		when(this.mock.get(anyInt())).thenReturn("toddy");
 
-		assertEquals("toddy", mock.get(5));
-		assertEquals("toddy", mock.get(10));
+		assertEquals("toddy", this.mock.get(5));
+		assertEquals("toddy", this.mock.get(10));
+	}
+
+	@Test
+	public void verificationBasics() {
+
+		this.mock.get(0);
+
+		verify(this.mock).get(anyInt());
+		verify(this.mock, times(1)).get(anyInt());
+		verifyNoMoreInteractions(this.mock);
 	}
 
 }
